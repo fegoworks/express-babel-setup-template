284571d861deb0c180c3b08b57c0c8b8d3bf0bce
unknown <fegoworks@hotmail.com>
2020-07-02 23:57:46 +0100

    feat(user): set up models and migrations

---
+++ src/migrations/20200702204841-create-user.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+      unique: true,
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      required: true
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      required: true
+    },
+    email: {
+      type: Sequelize.STRING,
+      required: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      required: true
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
+};
---
+++ src/models/user.js
@@ -0,0 +1,42 @@
+import {
+  hashPassword
+} from '../../helpers/utils';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      required: true,
+      primaryKey: true
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      required: true,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      required: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      required: true,
+    }
+  }, {});
+  User.associate = (models) => {
+    User.hasMany(models.Dummy, {
+      foreignKey: 'id',
+      as: 'userId',
+    });
+
+    User.beforeCreate(async (newUser) => {
+      newUser.password = '' || hashPassword(newUser.password);
+    });
+  };
+  return User;
+};
